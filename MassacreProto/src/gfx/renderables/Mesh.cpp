e9ad5e4a947b1d51c86b3955f0dd486491343b70
Dark_Confidant <git@dark.li>
2013-04-22 06:11:05 +0800

    Fix SimpleMesh attribute type conversion

--- MassacreProto/src/gfx/renderables/Mesh.cpp
+++ MassacreProto/src/gfx/renderables/Mesh.cpp
@@ -61,7 +61,7 @@ void Mesh::load(IFile* file)
     file->read(attribs, header.numAttributes);
 
     for (uint i = 0; i < header.numAttributes; ++i)
-        fmt.addAttrib(GType(attribs[i].type), attribs[i].length);
+        fmt.addAttrib(GType(GType::SByte + attribs[i].type), attribs[i].length); // sic!
 
     delete [] attribs;
 
