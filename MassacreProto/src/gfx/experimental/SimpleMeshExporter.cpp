6a99beca7976b5a6e6dffb6e3c1476609f181499
Dark_Confidant <git@dark.li>
2013-04-27 14:43:49 +0800

    Remove SimpleMesh importer and exporter headers

--- MassacreProto/src/gfx/experimental/SimpleMeshExporter.cpp
+++ MassacreProto/src/gfx/experimental/SimpleMeshExporter.cpp
@@ -1,5 +1,5 @@
 #include "Universe.h"
-#include "SimpleMeshExporter.h"
+#include <mcr/gfx/experimental/IMeshExporter.h>
 
 #include <fstream>
 #include <SimpleMesh4.h>
@@ -9,13 +9,6 @@ namespace gfx          {
 namespace experimental {
 
 namespace {
-const GLenum g_attribTypesTr[] =
-{
-    GL_BYTE,  GL_UNSIGNED_BYTE,
-    GL_SHORT, GL_UNSIGNED_SHORT,
-    GL_INT,   GL_UNSIGNED_INT,
-    GL_FLOAT, GL_DOUBLE
-};
 const uint g_primitiveTypeTr[] =
 {
     GL_POINT,
@@ -26,63 +19,73 @@ const uint g_primitiveTypeTr[] =
 };
 } // ns
 
+class SimpleMeshExporter: public IMeshExporter
+{
+public:
+    using RefCounted::operator new;
+    using RefCounted::operator delete;
+
+    MCR_INTERN SimpleMeshExporter() {}
+    MCR_INTERN ~SimpleMeshExporter() {}
+
+    MCR_INTERN bool export_(const Mesh& mesh, const char* filename) const
+    {
+        auto& fmt = mesh.buffer->format();
+
+        SimpleMesh::Header header;
+        header.signature     = 0x4d53u;
+        header.version       = SIMPLE_MESH_VERSION;
+        header.flags         = 0;
+        header.primitiveType = g_primitiveTypeTr[mesh.primitiveType];
+        header.vertexSize    = fmt.stride();
+        header.numAttributes = fmt.numAttribs();
+        header.numVertices   = mesh.numVertices;
+        header.numIndices    = mesh.numIndices;
+
+        header.attributeDataOffset = sizeof(header);
+        header.vertexDataOffset    = header.attributeDataOffset
+                                   + header.numAttributes * sizeof(SimpleMesh::VertexAttribute);
+        header.indexDataOffset     = header.vertexDataOffset
+                                   + header.numVertices * header.vertexSize;
+
+        std::ofstream file(filename, std::ios::binary);
+        if (!file)
+            return false;
+
+        file.write((const char*) &header, sizeof(header));
+        if (!file)
+            return false;
+
+
+        auto attribs = new SimpleMesh::VertexAttribute[header.numAttributes];
+
+        for (uint i = 0; i < header.numAttributes; ++i)
+        {
+            attribs[i].type   = fmt.attrib(i).type.type;
+            attribs[i].length = fmt.attrib(i).length;
+        }
+
+        file.write((const char*) attribs, header.numAttributes * sizeof(SimpleMesh::VertexAttribute));
+
+        delete [] attribs;
+
+        if (!file)
+            return false;
+
+
+        file.write((const char*) mesh.mapVertices(), header.numVertices * header.vertexSize);
+        file.write((const char*) mesh.mapIndices(), header.numIndices * sizeof(uint));
+
+        mesh.unmapVertices();
+        mesh.unmapIndices();
+
+        return !!file;
+    }
+};
+
 rcptr<IMeshExporter> createSimpleMeshExporter()
 {
     return new SimpleMeshExporter;
-}
-
-bool SimpleMeshExporter::export_(const Mesh& mesh, const char* filename) const
-{
-    auto& fmt = mesh.buffer->format();
-
-    SimpleMesh::Header header;
-    header.signature     = 0x4d53u;
-    header.version       = SIMPLE_MESH_VERSION;
-    header.flags         = 0;
-    header.primitiveType = g_primitiveTypeTr[mesh.primitiveType];
-    header.vertexSize    = fmt.stride();
-    header.numAttributes = fmt.numAttribs();
-    header.numVertices   = mesh.numVertices;
-    header.numIndices    = mesh.numIndices;
-
-    header.attributeDataOffset = sizeof(header);
-    header.vertexDataOffset    = header.attributeDataOffset
-                               + header.numAttributes * sizeof(SimpleMesh::VertexAttribute);
-    header.indexDataOffset     = header.vertexDataOffset
-                               + header.numVertices * header.vertexSize;
-
-    std::ofstream file(filename, std::ios::binary);
-    if (!file)
-        return false;
-
-    file.write((const char*) &header, sizeof(header));
-    if (!file)
-        return false;
-
-
-    auto attribs = new SimpleMesh::VertexAttribute[header.numAttributes];
-
-    for (uint i = 0; i < header.numAttributes; ++i)
-    {
-        attribs[i].type   = fmt.attrib(i).type.type;
-        attribs[i].length = fmt.attrib(i).length;
-    }
-
-    file.write((const char*) attribs, header.numAttributes * sizeof(SimpleMesh::VertexAttribute));
-
-    delete [] attribs;
-
-    if (!file)
-        return false;
-
-
-    file.write((const char*) mesh.mapVertices(), header.numVertices * header.vertexSize);
-    file.write((const char*) mesh.mapIndices(), header.numIndices * sizeof(uint));
-
-    mesh.unmapVertices();
-    mesh.unmapIndices();
-
-    return !!file;
 }
 
 } // ns experimental
--- MassacreProto/src/gfx/experimental/SimpleMeshImporter.cpp
+++ MassacreProto/src/gfx/experimental/SimpleMeshImporter.cpp
@@ -1,71 +1,100 @@
 #include "Universe.h"
-#include "SimpleMeshImporter.h"
+#include <mcr/gfx/experimental/IMeshImporter.h>
+
+#include <SimpleMesh4.h>
 
 namespace mcr          {
 namespace gfx          {
 namespace experimental {
 
+class SimpleMeshImportTask: public IMeshImportTask
+{
+public:
+    using RefCounted::operator new;
+    using RefCounted::operator delete;
+
+    MCR_INTERN SimpleMeshImportTask(IFile* file): m_file(file) {}
+
+    MCR_INTERN bool SimpleMeshImportTask::estimate(MeshInfo& infoOut)
+    {
+        if (m_file->read(m_header) != sizeof(m_header))
+            return false;
+
+        if (!m_header.checkSignature() || m_header.version != SIMPLE_MESH_VERSION)
+            return false;
+
+        auto attribs = new SimpleMesh::VertexAttribute[m_header.numAttributes];
+
+        m_file->seek(m_header.attributeDataOffset);
+        m_file->read(attribs, m_header.numAttributes);
+
+        for (uint i = 0; i < m_header.numAttributes; ++i)
+            m_info.vertexFormat.addAttrib(GType(GType::SByte + attribs[i].type), attribs[i].length); // sic!
+
+        delete [] attribs;
+
+        m_info.numVertices = m_header.numVertices;
+        m_info.numIndices  = m_header.numIndices;
+
+        infoOut = m_info;
+        return true;
+    }
+
+    MCR_INTERN bool import(const VertexFormat& bufferFormat, Mesh& mesh)
+    {
+        // not capable of converting
+        if (m_info.vertexFormat.numAttribs() != bufferFormat.numAttribs())
+            return false;
+
+        for (uint i = 0; i < bufferFormat.numAttribs(); ++i)
+        {
+            auto &myAttrib  = m_info.vertexFormat.attrib(i),
+                 &refAttrib = bufferFormat.attrib(i);
+
+            if (myAttrib.type != refAttrib.type || myAttrib.length != refAttrib.length)
+                return false;
+        }
+
+        size_t written = 0;
+
+        auto vertices = mesh.mapVertices(GBuffer::ReplaceSome);
+        auto indices  = mesh.mapIndices (GBuffer::ReplaceSome);
+
+        m_file->seek(m_header.vertexDataOffset);
+        written += m_file->read(vertices, m_header.numVertices * m_header.vertexSize);
+
+        m_file->seek(m_header.indexDataOffset);
+        written += m_file->read(indices, m_header.numIndices);
+
+        mesh.unmapVertices();
+        mesh.unmapIndices();
+
+        return written == m_header.numVertices * m_header.vertexSize
+                        + m_header.numIndices  * sizeof(uint);
+    }
+
+private:
+    rcptr<IFile> m_file;
+    MeshInfo m_info;
+    SimpleMesh::Header m_header;
+};
+
+
+class SimpleMeshImporter: public IMeshImporter
+{
+public:
+    using RefCounted::operator new;
+    using RefCounted::operator delete;
+
+    MCR_INTERN rcptr<IMeshImportTask> createTask(IFile* file) const
+    {
+        return new SimpleMeshImportTask(file);
+    }
+};
+
 rcptr<IMeshImporter> createSimpleMeshLoader()
 {
     return new SimpleMeshImporter;
-}
-
-bool SimpleMeshImportTask::estimate(MeshInfo& infoOut)
-{
-    if (m_file->read(m_header) != sizeof(m_header))
-        return false;
-
-    if (!m_header.checkSignature() || m_header.version != SIMPLE_MESH_VERSION)
-        return false;
-
-    auto attribs = new SimpleMesh::VertexAttribute[m_header.numAttributes];
-
-    m_file->seek(m_header.attributeDataOffset);
-    m_file->read(attribs, m_header.numAttributes);
-
-    for (uint i = 0; i < m_header.numAttributes; ++i)
-        m_info.vertexFormat.addAttrib(GType(GType::SByte + attribs[i].type), attribs[i].length); // sic!
-
-    delete [] attribs;
-
-    m_info.numVertices = m_header.numVertices;
-    m_info.numIndices  = m_header.numIndices;
-
-    infoOut = m_info;
-    return true;
-}
-
-bool SimpleMeshImportTask::import(const VertexFormat& bufferFormat, Mesh& mesh)
-{
-    // not capable of converting
-    if (m_info.vertexFormat.numAttribs() != bufferFormat.numAttribs())
-        return false;
-
-    for (uint i = 0; i < bufferFormat.numAttribs(); ++i)
-    {
-        auto &myAttrib  = m_info.vertexFormat.attrib(i),
-             &refAttrib = bufferFormat.attrib(i);
-
-        if (myAttrib.type != refAttrib.type || myAttrib.length != refAttrib.length)
-            return false;
-    }
-
-    size_t written = 0;
-
-    auto vertices = mesh.mapVertices(GBuffer::ReplaceSome);
-    auto indices  = mesh.mapIndices (GBuffer::ReplaceSome);
-
-    m_file->seek(m_header.vertexDataOffset);
-    written += m_file->read(vertices, m_header.numVertices * m_header.vertexSize);
-
-    m_file->seek(m_header.indexDataOffset);
-    written += m_file->read(indices, m_header.numIndices);
-
-    mesh.unmapVertices();
-    mesh.unmapIndices();
-
-    return written == m_header.numVertices * m_header.vertexSize
-                    + m_header.numIndices  * sizeof(uint);
 }
 
 } // ns experimental
