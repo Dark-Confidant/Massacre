1adf7d4a79309287d6c3058e932c244fadc1965f
Dark_Confidant <git@dark.li>
2013-05-08 18:01:04 +0800

    Fix build³

--- MassacreProto/src/gfx/MaterialParameterUploadFn.h
+++ MassacreProto/src/gfx/MaterialParameterUploadFn.h
@@ -10,25 +10,25 @@
 #define MCR_UPLOAD_AUXARGS_VEC() 1,
 #define MCR_UPLOAD_AUXARGS_MAT() 1, GL_FALSE,
 
-#define MCR_PARAMS                                  \
-    ((float,  1f,         BOOST_PP_EMPTY))          \
-    ((double, 1d,         BOOST_PP_EMPTY))          \
-    ((int,    1i,         BOOST_PP_EMPTY))          \
-    ((uint,   1ui,        BOOST_PP_EMPTY))          \
-    ((vec2,   2fv,        MCR_UPLOAD_AUXARGS_VEC))  \
-    ((dvec2,  2dv,        MCR_UPLOAD_AUXARGS_VEC))  \
-    ((ivec2,  2iv,        MCR_UPLOAD_AUXARGS_VEC))  \
-    ((uvec2,  2uiv,       MCR_UPLOAD_AUXARGS_VEC))  \
-    ((vec3,   3fv,        MCR_UPLOAD_AUXARGS_VEC))  \
-    ((dvec3,  3dv,        MCR_UPLOAD_AUXARGS_VEC))  \
-    ((ivec3,  3iv,        MCR_UPLOAD_AUXARGS_VEC))  \
-    ((uvec3,  3uiv,       MCR_UPLOAD_AUXARGS_VEC))  \
-    ((vec4,   4fv,        MCR_UPLOAD_AUXARGS_VEC))  \
-    ((dvec4,  4dv,        MCR_UPLOAD_AUXARGS_VEC))  \
-    ((ivec4,  4iv,        MCR_UPLOAD_AUXARGS_VEC))  \
-    ((uvec4,  4uiv,       MCR_UPLOAD_AUXARGS_VEC))  \
-    ((mat4,   Matrix4fv,  MCR_UPLOAD_AUXARGS_MAT))  \
-    ((dmat4,  Matrix4dv,  MCR_UPLOAD_AUXARGS_MAT))
+#define MCR_PARAMS                                      \
+    ((float)  (1f)         (BOOST_PP_EMPTY))            \
+    ((double) (1d)         (BOOST_PP_EMPTY))            \
+    ((int)    (1i)         (BOOST_PP_EMPTY))            \
+    ((uint)   (1ui)        (BOOST_PP_EMPTY))            \
+    ((vec2)   (2fv)        (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((dvec2)  (2dv)        (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((ivec2)  (2iv)        (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((uvec2)  (2uiv)       (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((vec3)   (3fv)        (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((dvec3)  (3dv)        (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((ivec3)  (3iv)        (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((uvec3)  (3uiv)       (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((vec4)   (4fv)        (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((dvec4)  (4dv)        (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((ivec4)  (4iv)        (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((uvec4)  (4uiv)       (MCR_UPLOAD_AUXARGS_VEC))    \
+    ((mat4)   (Matrix4fv)  (MCR_UPLOAD_AUXARGS_MAT))    \
+    ((dmat4)  (Matrix4dv)  (MCR_UPLOAD_AUXARGS_MAT))
 
 namespace mcr    {
 namespace gfx    {
@@ -42,15 +42,15 @@ void uploadParameter(uint program, int loc, const void* mem);
 //////////////////////////////////////////////////////////////////////////
 // Indirect parameter upload, assuming prior glUseProgram call
 
-#define MCR_UPLOAD_FN_SPEC(r, data, t)                                  \
-    template <>                                                         \
-    void uploadParameter<BOOST_PP_TUPLE_ELEM(2, 0, t), false>           \
-        (uint program, int loc, const void* mem)                        \
-    {                                                                   \
-        (void) program;                                                 \
-        BOOST_PP_CAT(glUniform, BOOST_PP_TUPLE_ELEM(2, 1, t))(          \
-            loc, BOOST_PP_TUPLE_ELEM(2, 2, t)()                         \
-            *static_cast<const BOOST_PP_TUPLE_ELEM(2, 0, t)*>(mem));    \
+#define MCR_UPLOAD_FN_SPEC(r, data, t)                          \
+    template <>                                                 \
+    void uploadParameter<BOOST_PP_SEQ_ELEM(0, t), false>        \
+        (uint program, int loc, const void* mem)                \
+    {                                                           \
+        (void) program;                                         \
+        BOOST_PP_CAT(glUniform, BOOST_PP_SEQ_ELEM(1, t))(       \
+            loc, BOOST_PP_SEQ_ELEM(2, t)()                      \
+            *static_cast<const BOOST_PP_SEQ_ELEM(0, t)*>(mem)); \
     }
 
 BOOST_PP_SEQ_FOR_EACH(MCR_UPLOAD_FN_SPEC, ~, MCR_PARAMS)
@@ -61,14 +61,14 @@ BOOST_PP_SEQ_FOR_EACH(MCR_UPLOAD_FN_SPEC, ~, MCR_PARAMS)
 //////////////////////////////////////////////////////////////////////////
 // Direct parameter upload via EXT_direct_state_access functions
 
-#define MCR_UPLOAD_FN_SPEC(r, data, t)                                  \
-    template <>                                                         \
-    void uploadParameter<BOOST_PP_TUPLE_ELEM(2, 0, t), true>            \
-        (uint program, int loc, const void* mem)                        \
-    {                                                                   \
-        BOOST_PP_CAT(glProgramUniform, BOOST_PP_TUPLE_ELEM(2, 1, t))(   \
-            program, loc, BOOST_PP_TUPLE_ELEM(2, 2, t)()                \
-            *static_cast<const BOOST_PP_TUPLE_ELEM(2, 0, t)*>(mem));    \
+#define MCR_UPLOAD_FN_SPEC(r, data, t)                              \
+    template <>                                                     \
+    void uploadParameter<BOOST_PP_SEQ_ELEM(0, t), true>             \
+        (uint program, int loc, const void* mem)                    \
+    {                                                               \
+        BOOST_PP_CAT(glProgramUniform, BOOST_PP_SEQ_ELEM(1, t))(    \
+            program, loc, BOOST_PP_SEQ_ELEM(2, t)()                 \
+            *static_cast<const BOOST_PP_SEQ_ELEM(0, t)*>(mem));     \
     }
 
 BOOST_PP_SEQ_FOR_EACH(MCR_UPLOAD_FN_SPEC, ~, MCR_PARAMS)
@@ -84,7 +84,7 @@ namespace {
 // choose one according to presence of the above extension
 
 #define MCR_UPLOAD_FN_ENTRY_SPEC(r, direct, t) \
-    &detail::uploadParameter<BOOST_PP_TUPLE_ELEM(2, 0, t), direct>,
+    &detail::uploadParameter<BOOST_PP_SEQ_ELEM(0, t), direct>,
 
 void(*const g_uploadFnTableIndirect[])(uint, int, const void*) =
 {
