ee28bf0a24b4de850b17afe3f589cc724dc60ec0
Dark_Confidant <git@dark.li>
2013-06-15 18:40:15 +0800

    Add trailing zero bit counting function for later use

--- Core/include/mcr/math/Math.h
+++ Core/include/mcr/math/Math.h
@@ -31,5 +31,17 @@ inline const T& clamp(const T& a, const T& min, const T& max)
     return std::max(min, std::min(a, max));
 }
 
+inline byte numTrailingZeroes(uint a)
+{
+    static const byte s_mod37zeroes[] =
+    {
+        32, 0, 1, 26, 2, 23, 27, 0, 3, 16, 24, 30, 28, 11, 0, 13,
+        4, 7, 17, 0, 25, 22, 31, 15, 29, 10, 12, 6, 0, 21, 14, 9,
+        5, 20, 8, 19, 18
+    };
+
+    return s_mod37zeroes[(a & (0u - a)) % 37u];
+}
+
 } // ns math
 } // ns mcr
